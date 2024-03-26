79ca7c8cb1cabd2cbd13455e6504878248db5042
Seeker223 <baduntobi2024@gmail.com>
2024-03-26 13:09:03 +0100

    second commit

---
+++ src/components/Cat.js
@@ -0,0 +1,12 @@
+import { Avatar } from '@mui/material';
+import React from 'react';
+
+const Cat = () => {
+    return (
+        <Avatar scr="https://www.pexels.com/photo/low-angle-shot-of-a-tabby-cat-208984"/>
+
+    )
+    
+}
+
+export default Cat
