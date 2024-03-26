955d2a8ab56c2d0c8e248e3135cffe70da202498
Seeker223 <baduntobi2024@gmail.com>
2024-03-26 14:19:06 +0100

    updated iconButtons

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,9 +1,9 @@
-import { Box } from "@mui/material";
+import { Box, Stack, IconButton } from "@mui/material";
 import {useTheme} from "@mui/material/styles"
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Logo from "../../assets/Images/logo.ico"
-
+import {Nav_Buttons} from "../../data/index.js";
 const DashboardLayout = () => {
 
 const theme = useTheme();
@@ -11,21 +11,35 @@ const theme = useTheme();
   return (
     <>
 <Box 
+p={2}
   sx={{
         backgroundColor: theme.palette.background.paper, 
         boxShadow: "0px 0px 2px  rgba(0, 0, 0, 0.25)", 
         height:"100vh",
-         width:"100px" }}>
-<Box 
+         width: 100 }}>
+
+  <Stack direction="column" alignItems={"center"} sx={{width: "100%"}}>
+  <Box 
   sx={{
-  backgroundColor: theme.palete.primary.main,
+  backgroundColor: theme.palette.primary.main,
   height: 64,
   width: 64,
   borderRadius: 1.5,
 }}>
+
 <img src={Logo} alt={"Chat App Logo"} />
 </Box>
+  <Stack>
+  {Nav_Buttons.map((el) => (
+       <IconButton key={el.index}>
+        {el.icon }
+       </IconButton>))}
+
+  </Stack>
+      
+       </Stack>
       </Box>
+
       <Outlet />
     </>
   );
