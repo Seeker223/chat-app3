3d0893e5210b01a0e9f7b61f647d7c5bf1a8bc7d
Seeker223 <baduntobi2024@gmail.com>
2024-03-27 11:25:40 +0100

    first commit

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,113 +1,21 @@
-import { Box, Stack, IconButton, Divider, Avatar } from "@mui/material";
-import { useTheme } from "@mui/material/styles"
-import React, { useState } from "react";
+import { Box } from "@mui/material";
+import React from "react";
 import { Outlet } from "react-router-dom";
-import Logo from "../../assets/Images/logo.ico"
-import { Nav_Buttons } from "../../data/index.js";
-import { Gear } from "phosphor-react";
-import { faker } from '@faker-js/faker';
+
 
 const DashboardLayout = () => {
-
-  const theme = useTheme();
-
-  const [selected, setSelected] = useState(0);
-
-  console.log(theme);
   return (
     <>
-      <Box
-        p={2}
-        sx={{
-          backgroundColor: theme.palette.background.paper,
-          boxShadow: "0px 0px 2px  rgba(0, 0, 0, 0.25)",
-          height: "100vh",
-          width: 100
-        }}>
+      <Box sx={{
+        backgroundColor: "black",
+        height: "100vh",
+        width: 100
+      }}>
 
-        <Stack direction="column" alignItems={"center"} sx={{ width: "100%" }} spacing={3}>
-          <Box
-            sx={{
-              backgroundColor: theme.palette.primary.main,
-              height: 64,
-              width: 64,
-              borderRadius: 1.5,
-            }}>
-
-            <img src={Logo} alt={"Chat App Logo"} />
-          </Box>
-          <Stack
-            sx={{
-              width: "max-content"
-            }}
-            direction="column" alignItems="center"
-            spacing={3}>
-
-            {Nav_Buttons.map((el) => (
-              el.index === selected ?
-                <Box
-                  p={1}
-                  sx={{
-                    backgroundColor: theme.palette.primary.main,
-                    borderRadius: 1.5,
-                  }}
-                >
-
-                  <IconButton sx={{
-                    width: "max-content",
-                    color: "#fff"
-                  }}
-                    key={el.index}>
-                    {el.icon}
-                  </IconButton>
-                </Box>
-                :
-                <IconButton
-                  onClick={() => {
-                    setSelected(el.index);
-                  }}
-                  sx={{
-                    width: "max-content",
-                    color: "#000"
-                  }}
-                  key={el.index}
-                >
-                  {el.icon}
-                </IconButton>
-            )
-            )}
-            <Divider sx={{ width: "48px" }} />
-            {selected === 3 ?
-              <Box
-                p={1}
-                sx={{
-                  backgroundColor: theme.palette.primary.main,
-                  borderRadius: 1.5,
-                }}
-              >
-                <IconButton
-                  sx={{ width: "max-content", color: "#fff" }}>
-                  <Gear />
-                </IconButton>
-              </Box>
-
-              : <IconButton onClick={() => {
-                setSelected(3);
-              }}
-              >
-                sx={{ width: "max-content", color: "#000" }}
-                <Gear />
-              </IconButton>
-            }
-          </Stack>
-          <Stack>
-            <Avatar src={faker.image.avatar()} />
-          </Stack>
-        </Stack>
       </Box>
       <Outlet />
     </>
-  );
-};
+  )
+}
 
 export default DashboardLayout;
