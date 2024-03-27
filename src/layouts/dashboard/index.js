be42baed16ae533fd3bfe6a0ee277dbf6aa6b760
Seeker223 <baduntobi2024@gmail.com>
2024-03-27 13:27:28 +0100

    updated with mui toggle button

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,5 +1,5 @@
-import { Box, Stack, IconButton, Divider, Avatar } from "@mui/material";
-import { useTheme } from "@mui/material/styles"
+import { Box, Stack, IconButton, Divider, Avatar, Switch } from "@mui/material";
+import { useTheme, styled } from "@mui/material/styles"
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Logo from "../../assets/Images/logo.ico"
@@ -7,6 +7,47 @@ import { Nav_Buttons } from "../../data/index.js";
 import { Gear } from "phosphor-react";
 import {faker} from '@faker-js/faker';
 
+const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 28,
+  height: 16,
+  padding: 0,
+  display: 'flex',
+  '&:active': {
+    '& .MuiSwitch-thumb': {
+      width: 15,
+    },
+    '& .MuiSwitch-switchBase.Mui-checked': {
+      transform: 'translateX(9px)',
+    },
+  },
+  '& .MuiSwitch-switchBase': {
+    padding: 2,
+    '&.Mui-checked': {
+      transform: 'translateX(20px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#177ddc' : '#1890ff',
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    transition: theme.transitions.create(['width'], {
+      duration: 200,
+    }),
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 20 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === 'dark' ? 'rgba(255,255,255,.35)' : 'rgba(0,0,0,.25)',
+    boxSizing: 'border-box',
+  },
+}));
 
 const DashboardLayout = () => {
   const theme = useTheme();
@@ -25,7 +66,8 @@ const DashboardLayout = () => {
         <Stack 
         direction="column" 
         alignItems={"center"} 
-        sx={{ width: "100%" }} 
+        justifyContent="space-between"
+        sx={{ height: "100%" }} 
         spacing={3}>
          
          <Stack alignItems={"center"} spacing={4}>
@@ -104,7 +146,8 @@ const DashboardLayout = () => {
          </Stack>
 
           
-          <Stack>
+          <Stack spacing={4}>
+            <AntSwitch defaultChecked/>
           <Avatar  src={faker.image.avatar()}/>
           </Stack>
         </Stack>
